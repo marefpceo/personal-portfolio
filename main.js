53e564b6f53dc04b3c900e6559ca9375d12104c7
marefpceo <32721274+marefpceo@users.noreply.github.com>
2023-07-26 21:04:30 -0700

    Changed animations to transitions
    Header and welcome-section animations were changed to transitions.

--- main.js
+++ main.js
@@ -1,22 +1,46 @@
 const header = document.querySelector('header');
 const welcomeSection = document.getElementById('welcome-section');
+const greeting = document.getElementById('greeting');
+const displayName = document.getElementById('name');
+const jobTitle = document.getElementById('job-title');
+const mainPhoto = document.getElementById('main-photo');
 
 function displayHeader() {
-  if (window.scrollY > 130) {
-    header.style.display = 'flex';
-    header.style.boxShadow = '0 1px 4px 0px var(--text-primary)';
+  if (window.scrollY > 100) {
+    header.style.opacity = 1;
     welcomeSection.style.height = '75vh';
-    welcomeSection.style.animation = 'welcome-collapse 2s';
   } else {
-    header.style.display = 'none';
-    header.style.boxShadow = '';
-    welcomeSection.style.animation = 'welcome-expand 3s';
+    header.style.opacity = 0;
     welcomeSection.style.height = '100vh';
   }
-  
 }
+
+function welcomeTitleLoad() {
+  greeting.style.transform = 'translate(0%, 0%)';
+  greeting.style.opacity = 1;
+  greeting.style.transition = 'opacity 4s ease-out, transform 4s ease-out';
+  displayName.style.transform = 'scale(1) translate(0%, 0%)';
+  displayName.style.opacity = 1;
+  displayName.style.transition = 'opacity 6s ease-out, transform 6s ease-out';
+  jobTitle.style.transform = 'translate(0%, 0%)';
+  jobTitle.style.opacity = 1;
+  jobTitle.style.transition = 'opacity 5s ease-out, transform 5s ease-out';  
+  mainPhoto.style.opacity = 1;
+  mainPhoto.style.transition = 'opacity 4s ease 4s';
+}
+
+window.addEventListener('load', () => {
+  welcomeTitleLoad();
+});
 
 window.addEventListener('scroll', () => {
   displayHeader();
-})
+});
 
+
+
+
+
+
+
+  
